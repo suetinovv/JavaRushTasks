1b01df21d554697871a09487ee9615babc9362ec
Vladimir <nikalaich02@yandex.ru>
2022-08-20 23:30:22 +0300

    add Task01

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0105;
+
+/* 
+Объявляем переменные
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // объявляем (создаем) переменную name типа String
+        String name;
+        // объявляем (создаем) переменную age типа int
+        int age;
+        // объявляем (создаем) переменную city типа String
+        String city;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0142/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0142;
+
+/* 
+Амиго очень умный
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        // выводим фразу "Амиго очень умный" на экран, кавычки выведены не будут
+        System.out.print("Амиго очень умный");
+    }
+}
