2eca3e8ad595ec003b0d1688926de16f57ee44e5
Vladimir <nikalaich02@yandex.ru>
2022-08-21 17:56:18 +0300

    add Task06

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        int[] array2 = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array2.length; i++) {
+            array[i] = array2[(array2.length - i - 1)];
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int x){
+
+    }
+    public static void universalMethod(String s){
+
+    }
+    public static void universalMethod(int x, String s){
+
+    }
+    public static void universalMethod(String s, int x){
+
+    }
+    public static void universalMethod(boolean b){
+
+    }
+    public static void universalMethod(char c){
+
+    }
+    public static void universalMethod(int x, boolean b){
+
+    }
+    public static void universalMethod(int x, char c){
+
+    }
+    public static void universalMethod(int x, boolean b, char c){
+
+    }
+}
