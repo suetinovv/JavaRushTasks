0b5cbd91e82adc1eb2e9ab751d95c4d0f8ebc5f1
Vladimir <nikalaich02@yandex.ru>
2022-08-29 17:17:28 +0300

    add 3.JavaMultithreading Task24

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+
+    public void metod1(){
+
+    }
+    public void metod2(){
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2406/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task24.task2406;
+
+import java.math.BigDecimal;
+
+/* 
+Наследование от внутреннего класса
+*/
+
+public class Solution {
+    public static class Building {
+        public static class Hall {
+            private BigDecimal square;
+
+            public Hall(BigDecimal square) {
+                this.square = square;
+            }
+        }
+
+        public static class Apartments {
+        }
+    }
+
+    public class Apt3Bedroom extends Building.Apartments {
+
+    }
+    public class BigHall extends Building.Hall {
+
+        public BigHall(BigDecimal square) {
+            super(square);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
